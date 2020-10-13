9577cd7343ad523440c83ba7b8dd33fb557c5639
quan <quan_zhezhu@aliber.co.jp>
2020-10-13 09:51:33 +0900

    社員編集画面更新しました

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('employee_list', views.EmployeeListView.as_view(), name="employee_list"),
     path('employee', views.EmployeeView.as_view(), name="employee"),
     path('employee2/<int:pk>/', views.EmployeeView2.as_view(), name="employee2"),
+    path('employee_detail/<int:pk>/', views.EmployeeDetailView.as_view(), name="employee_detail"),
+    path('employee_update/<int:pk>/', views.EmployeeUpdateView.as_view(), name="employee_update"),
 ]
