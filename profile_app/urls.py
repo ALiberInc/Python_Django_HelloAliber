3a56b7ec104c2e944474cb67cdb557290a775db7
NingWen-In-Hub <ning_wen@aliber.co.jp>
2020-10-21 17:25:43 +0900

    1.Profile削除機能を追加する 2.社員詳細画面 　生年月日から年齢を算出する 　一般社員が他人の詳細画面にアクセスできないチェックを追加する

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('employee_list', views.EmployeeListView.as_view(), name="employee_list"),
     path('employee/<int:pk>/', views.EmployeeView.as_view(), name="employee"),
+    path('employee_delete/<int:pk>/', views.EmployeeDeleteView, name="employee_delete"),
 ]
--- profile_app/views.py
+++ profile_app/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, resolve_url
+from django.views.decorators.http import require_POST
 
 # モデル
 from .models import *
@@ -21,7 +24,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 
-from datetime import timedelta
+# timeについて
+import datetime
+from django.utils import timezone
 
 # EmployeeCreateView用（MySignUp）
 from allauth.account.views import SignupView
@@ -39,15 +44,18 @@ logger = logging.getLogger(__name__)
 
 def lastname(request):
     """共通画面用user_id,lastname"""
+    user_id = -1
+    is_staff = False
     try:
         logger.debug('ユーザー：{}'.format(request.user))
         data = Profile.objects.get(id_id__exact=request.user.id)
         user_id = Profile.objects.get(id_id__exact=request.user.id).user_id
+        is_staff = request.user.is_staff
     except:
         logger.debug('profileにデータを登録していない')
         data = 'データ無し'
-        user_id = -1
-    return {'common_last_name': data ,'user_id': user_id}
+    return {'common_last_name': data ,'user_id': user_id, 'common_is_staff': is_staff
+            ,'date_today': datetime.date.today()}
 
 
 class IndexView(generic.TemplateView):
@@ -63,7 +71,7 @@ class EmployeeListView(generic.ListView):
     paginate_by = 6
 
     def get_queryset(self):
-        profiles = Profile.objects.order_by('user_id')
+        profiles = Profile.objects.filter(delete=0).order_by('user_id')
         return profiles    
 
 
@@ -71,3 +79,39 @@ class EmployeeView(generic.DetailView, LoginRequiredMixin):
     """社員詳細画面"""
     model = Profile
     template_name = "ENP002_employee.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        birth = Profile.objects.get(user_id=self.kwargs['pk']).birth # pkを指定してデータを絞り込む
+        logger.debug('私は{}だよねー'.format(birth))
+        time0 = int("".join(str(birth).split("-")))
+
+        time = str(datetime.date.today())
+        now_time = int("".join(time.split("-")))
+        age = int(((now_time - time0) / 10000))+1
+        context['count_age'] = age
+        return context
+
+@require_POST
+def EmployeeDeleteView(request, pk):
+    logger.debug("pk={}".format(pk))
+    employee = get_object_or_404(CustomUser, id=pk) #データが存在していることを確認する
+    profile = get_object_or_404(Profile, id_id=pk) #同上
+    if employee and profile:
+        logger.debug("ユーザーを削除している：　no.{} name.{}".format(pk, profile))
+        if request.user.is_staff: # 管理者権限を確認する
+            if request.user.id == pk:
+                logger.info("自分のデータを削除できません。")
+                messages.add_message(request, messages.ERROR, '自分のデータを削除できません。')
+                return redirect(request.META['HTTP_REFERER'])
+            else:
+                CustomUser.objects.filter(id=pk).update(is_active = 0) #CustomUserの削除
+                Profile.objects.filter(id_id=pk).update(delete = 1, update_date = timezone.now(),
+                 update_id = request.user.id) #Profileの削除
+        else:
+            logger.info("管理者のみprofileを削除することができる")
+            raise PermissionDenied # 権限なし
+
+    response = redirect('profile_app:employee_list')
+    return response
+
