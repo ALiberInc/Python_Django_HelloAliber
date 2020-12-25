a7e28375585445196b2e0cb30816a5b1c928f0b2
NingWen-In-Hub <ning_wen@aliber.co.jp>
2020-12-25 12:14:01 +0900

    ENP004社員編集 　バグ修正 　クライアント側検証からサーバー側の検証に修正する ENP002社員詳細 　CSS修正 　「非アクティブ」の背景色を消す ENP001社員一覧 　バグ修正 　非アクティブユーザーを表示させるように修正する

--- profile_app/forms.py
+++ profile_app/forms.py
@@ -8,6 +8,7 @@ import logging
 import json
 import sys
 import requests
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -19,12 +20,13 @@ class ProfileCreateForm(ModelForm):
 class ProfileEditForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ("user", "created_at", "updated_at","user_id","delete","create_id","update_id","gender")
+        exclude = ("user", "created_at", "updated_at","user_id","delete","create_id","update_id","gender", "birth")
     email = forms.EmailField(initial='',label='メールアドレス', required=True,)
     gender_CHOICES=[('1','男性'),('0','女性'),]
     gender = forms.CharField(
         label='性別', 
         widget=forms.RadioSelect(choices=gender_CHOICES))
+    birth = forms.CharField(label="生年月日", required=True)
     is_active = forms.CharField(
         label='アクティブ', 
         widget=forms.RadioSelect(choices=[('True','アクティブ'),('False','非アクティブ'),]))
@@ -33,67 +35,31 @@ class ProfileEditForm(ModelForm):
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # for field in self.fields.values():
-        #     field.widget.attrs["class"] = "form-control"
         self.fields['id'].widget = forms.HiddenInput()
-        self.fields['last_name_k'].widget.attrs['pattern'] = '^[ア-ン゛゜ァ-ォャ-ョー「」、]+$'
         self.fields['last_name_k'].required = True
-        self.fields['last_name_k'].error_messages = {'required': self.fields['last_name_k'].label+'を入力してください。'}
-        self.fields['first_name_k'].widget.attrs['pattern'] = '^[ア-ン゛゜ァ-ォャ-ョー「」、]+$'
         self.fields['first_name_k'].required = True
-        self.fields['first_name_k'].error_messages = {'required': self.fields['first_name_k'].label+'を入力してください。'}
-        self.fields['last_name'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
-        self.fields['last_name'].error_messages = {'required': self.fields['last_name'].label+'を入力してください。'}
-        self.fields['first_name'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
-        self.fields['first_name'].error_messages = {'required': self.fields['first_name'].label+'を入力してください。'}
         self.fields['gender'].widget.attrs["class"] = "gender_class"
-        self.fields['email'].widget.attrs['pattern'] = '^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$'
-        self.fields['email'].error_messages = {'required': self.fields['email'].label+'を入力してください。'}
-        self.fields['nationality'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['nationality'].required = True
-        self.fields['nationality'].error_messages = {'required': self.fields['nationality'].label+'を入力してください。'}
-        self.fields['phone'].widget.attrs['pattern'] = '^[0-9]+$'
         self.fields['phone'].required = True
-        self.fields['phone'].error_messages = {'required': self.fields['phone'].label+'を入力してください。'}
-        self.fields['postal_code'].widget.attrs['pattern'] = '^[0-9]+$'
         self.fields['postal_code'].required = True
-        self.fields['postal_code'].error_messages = {'required': self.fields['postal_code'].label+'を入力してください。'}
-        self.fields['address1'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['address1'].required = True
-        self.fields['address1'].error_messages = {'required': self.fields['address1'].label+'を入力してください。'}
         self.fields['address1'].widget.attrs['placeholder'] = '都道府県名、または市区町村名を入力してください'
-        self.fields['address2'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['address2'].required = True
-        self.fields['address2'].error_messages = {'required': self.fields['address2'].label+'を入力してください。'}
         self.fields['address2'].widget.attrs['placeholder'] = '町域名を入力してください'
-        self.fields['residence_card'].widget.attrs['pattern'] = '^[a-zA-Z0-9]+$'
-        # self.fields['residence_card'].required = True
-        self.fields['residence_card'].error_messages = {'required': self.fields['residence_card'].label+'を入力してください。'}
-        self.fields['health_insurance'].widget.attrs['pattern'] = '^[a-zA-Z0-9]+$'
         self.fields['health_insurance'].required = True
-        self.fields['health_insurance'].error_messages = {'required': self.fields['health_insurance'].label+'を入力してください。'}
         self.fields['department_pro'].widget.attrs['maxlength'] = '10'
-        self.fields['emergency_contact_1_name'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_1_name'].required = True
-        self.fields['emergency_contact_1_name'].error_messages = {'required': self.fields['emergency_contact_1_name'].label+'を入力してください。'}
-        self.fields['emergency_contact_1_relationship'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_1_relationship'].required = True
-        self.fields['emergency_contact_1_relationship'].error_messages = {'required': self.fields['emergency_contact_1_relationship'].label+'を入力してください。'}
-        self.fields['emergency_contact_1_phone'].widget.attrs['pattern'] = '^[0-9]+$'
         self.fields['emergency_contact_1_phone'].required = True
-        self.fields['emergency_contact_1_phone'].error_messages = {'required': self.fields['emergency_contact_1_phone'].label+'を入力してください。'}
         self.fields['emergency_contact_2_name'].widget.attrs['maxlength'] = '20'
-        self.fields['emergency_contact_2_name'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_2_relationship'].widget.attrs['maxlength'] = '10'
-        self.fields['emergency_contact_2_relationship'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_2_phone'].widget.attrs['maxlength'] = '15'
-        self.fields['emergency_contact_2_phone'].widget.attrs['pattern'] = '^[0-9]+$'
         self.fields['emergency_contact_3_name'].widget.attrs['maxlength'] = '20'
-        self.fields['emergency_contact_3_name'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_3_relationship'].widget.attrs['maxlength'] = '10'
-        self.fields['emergency_contact_3_relationship'].widget.attrs['pattern'] = '^[ぁ-んァ-ヶー一-龠]+$'
         self.fields['emergency_contact_3_phone'].widget.attrs['maxlength'] = '15'
-        self.fields['emergency_contact_3_phone'].widget.attrs['pattern'] = '^[0-9]+$'
+        for field in self.fields.values():
+            if field.required:
+                field.error_messages = {'required': field.label+'を入力してください。'}
     
     def clean_email(self):      
         email = self.cleaned_data.get('email')
@@ -102,7 +68,7 @@ class ProfileEditForm(ModelForm):
         
         if email and CustomUser.objects.filter(email=email).exclude(id=id).count():
             raise forms.ValidationError("メールアドレスが既に存在しました。")
-        elif len(email) > 30:
+        if len(email) > 30:
             raise forms.ValidationError("30桁以内を入力してください。")
         return self.cleaned_data["email"]
 
@@ -110,25 +76,37 @@ class ProfileEditForm(ModelForm):
         last_name_k = self.data.get('last_name_k')
         if len(last_name_k) > 20:
             raise forms.ValidationError("20桁以内を入力してください。")
+        elif not re.match(r"[ア-ン゛゜ァ-ォャ-ョー「」、]", last_name_k):
+            raise forms.ValidationError("姓（カタカナ）はカタカナだけを入力してください。")
         return self.cleaned_data["last_name_k"]
     
     def clean_first_name_k(self):
         first_name_k = self.data.get('first_name_k')
         if len(first_name_k) > 20:
             raise forms.ValidationError("20桁以内を入力してください。")
+        elif not re.match(r"[ア-ン゛゜ァ-ォャ-ョー「」、]", first_name_k):
+            raise forms.ValidationError("名（カタカナ）はカタカナだけを入力してください。")
         return self.cleaned_data["first_name_k"]
 
     def clean_last_name(self):
         last_name = self.data.get('last_name')
-        if len(last_name) > 10:
-            raise forms.ValidationError("10桁以内を入力してください。")
+        if len(last_name) > 15:
+            raise forms.ValidationError("15桁以内を入力してください。")
         return self.cleaned_data["last_name"]
 
     def clean_first_name(self):
         first_name = self.data.get('first_name')
-        if len(first_name) > 10:
-            raise forms.ValidationError("10桁以内を入力してください。")
+        if len(first_name) > 15:
+            raise forms.ValidationError("15桁以内を入力してください。")
         return self.cleaned_data["first_name"]
+
+    def clean_birth(self):
+        birth = self.data.get('birth')
+        if len(birth) > 10:
+            raise forms.ValidationError("10桁以内を入力してください。")
+        elif not re.match(r"\d{4}-\d{2}-\d{2}", birth):
+            raise forms.ValidationError("「yyyy-mm-dd」の形で入力してください。")
+        return self.cleaned_data["birth"]
 
     def clean_nationality(self):
         nationality = self.data.get('nationality')
@@ -140,12 +118,16 @@ class ProfileEditForm(ModelForm):
         phone = self.data.get('phone')
         if len(phone) > 15:
             raise forms.ValidationError("15桁以内を入力してください。")
+        elif not re.match(r"[0-9]", phone):
+            raise forms.ValidationError("「携帯電話」は数字だけを入力してください。")
         return self.cleaned_data["phone"]
 
     def clean_postal_code(self):
         postal_code = self.data.get('postal_code')
         if len(postal_code) > 10:
             raise forms.ValidationError("10桁以内を入力してください。")
+        elif not re.match(r"[0-9\-]", postal_code):
+            raise forms.ValidationError("「郵便番号」は数字だけを入力してください。")
         return self.cleaned_data["postal_code"]
 
     def clean_address1(self):
@@ -164,12 +146,16 @@ class ProfileEditForm(ModelForm):
         residence_card = self.data.get('residence_card')
         if len(residence_card) > 15:
             raise forms.ValidationError("15桁以内を入力してください。")
+        elif residence_card !="" and not re.match(r"[0-9a-zA-Z]", residence_card):
+            raise forms.ValidationError("「在留カード番号」は英数字だけを入力してください。")
         return self.cleaned_data["residence_card"]
 
     def clean_health_insurance(self):
         health_insurance = self.data.get('health_insurance')
         if len(health_insurance) > 10:
             raise forms.ValidationError("10桁以内を入力してください。")
+        elif not re.match(r"[0-9]", health_insurance):
+            raise forms.ValidationError("「健康保険番号」は数字だけを入力してください。")
         return self.cleaned_data["health_insurance"]
 
     def clean_emergency_contact_1_name(self):
@@ -188,5 +174,46 @@ class ProfileEditForm(ModelForm):
         emergency_contact_1_phone = self.data.get('emergency_contact_1_phone')
         if len(emergency_contact_1_phone) > 15:
             raise forms.ValidationError("15桁以内を入力してください。")
+        elif not re.match(r"[0-9]", emergency_contact_1_phone):
+            raise forms.ValidationError("「電話番号」は数字だけを入力してください。")
         return self.cleaned_data["emergency_contact_1_phone"]
-        
+
+    def clean_emergency_contact_2_name(self):
+        emergency_contact_2_name = self.data.get('emergency_contact_2_name')
+        if len(emergency_contact_2_name) > 20:
+            raise forms.ValidationError("20桁以内を入力してください。")
+        return self.cleaned_data["emergency_contact_2_name"]
+
+    def clean_emergency_contact_2_relationship(self):
+        emergency_contact_2_relationship = self.data.get('emergency_contact_2_relationship')
+        if len(emergency_contact_2_relationship) > 10:
+            raise forms.ValidationError("10桁以内を入力してください。")
+        return self.cleaned_data["emergency_contact_2_relationship"]
+
+    def clean_emergency_contact_2_phone(self):
+        emergency_contact_2_phone = self.data.get('emergency_contact_2_phone')
+        if len(emergency_contact_2_phone) > 15:
+            raise forms.ValidationError("15桁以内を入力してください。")
+        elif emergency_contact_2_phone != "" and not re.match(r"[0-9]", emergency_contact_2_phone):
+            raise forms.ValidationError("「電話番号」は数字だけを入力してください。")
+        return self.cleaned_data["emergency_contact_2_phone"]
+
+    def clean_emergency_contact_3_name(self):
+        emergency_contact_3_name = self.data.get('emergency_contact_3_name')
+        if len(emergency_contact_3_name) > 20:
+            raise forms.ValidationError("20桁以内を入力してください。")
+        return self.cleaned_data["emergency_contact_3_name"]
+
+    def clean_emergency_contact_3_relationship(self):
+        emergency_contact_3_relationship = self.data.get('emergency_contact_3_relationship')
+        if len(emergency_contact_3_relationship) > 10:
+            raise forms.ValidationError("10桁以内を入力してください。")
+        return self.cleaned_data["emergency_contact_3_relationship"]
+
+    def clean_emergency_contact_3_phone(self):
+        emergency_contact_3_phone = self.data.get('emergency_contact_3_phone')
+        if len(emergency_contact_3_phone) > 15:
+            raise forms.ValidationError("15桁以内を入力してください。")
+        elif emergency_contact_3_phone != "" and not re.match(r"[0-9]", emergency_contact_3_phone):
+            raise forms.ValidationError("「電話番号」は数字だけを入力してください。")
+        return self.cleaned_data["emergency_contact_3_phone"]
